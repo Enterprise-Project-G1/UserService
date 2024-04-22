409404838f0338c055575392306dee3028c50932
Tenzin Om <12210036.gcit@rub.edu.bt>
2024-04-22 19:24:09 +0000

    User creation and Role creation done

---
+++ userservice/src/main/java/bt/edu/gcit/userservice/dao/UserDAO.java
@@ -0,0 +1,7 @@
+package bt.edu.gcit.userservice.dao;
+
+import bt.edu.gcit.userservice.entity.User;
+
+public interface UserDAO {
+    User save(User user);
+}
---
+++ userservice/src/main/java/bt/edu/gcit/userservice/service/UserService.java
@@ -0,0 +1,7 @@
+package bt.edu.gcit.userservice.service;
+
+import bt.edu.gcit.userservice.entity.User;
+
+public interface UserService {
+    User save(User user);
+}
